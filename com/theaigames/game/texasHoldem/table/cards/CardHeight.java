9ce53f7d1b7ee70ddda6f9d8020f2c25827e51c4
jvaneeden <jim_v_eeden@hotmail.com>
2015-06-26 11:16:51 +0200

    added licensing

--- com/theaigames/game/texasHoldem/table/cards/CardHeight.java
+++ com/theaigames/game/texasHoldem/table/cards/CardHeight.java
@@ -1,4 +1,22 @@
+// Copyright 2015 theaigames.com (developers@theaigames.com)
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//	
+//    For the full copyright and license information, please view the LICENSE
+//    file that was distributed with this source code.
+
 package com.theaigames.game.texasHoldem.table.cards;
+
 public enum CardHeight {
 	DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
